d72ce3bc3712139b0a884c57bab58d4c2fe32b27
Pawan Shah <pawan.shah@studytafensw.edu.au>
2022-09-28 12:36:08 +1000

    dkkvvc

--- src/App.js
+++ src/App.js
@@ -1,1 +1,26 @@
+import { BrowserRouter ,Routes, Route} from 'react-router-dom';
+import Home from './views/Home';
+import Login from './views/Login';
+import './App.css';
+import Register from './views/Register';
+import Ticket from './views/tickets';
 
+function App() {
+  return (
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Home />}>
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+              <Route path='/tickets' element={<Ticket.Layout />} >
+                  <Route path="" element={<Ticket.List />}/>
+                  <Route path="create" element={<Ticket.Create />}/>
+              </Route>
+            </Route>
+        </Routes>      
+    </BrowserRouter>
+
+  );
+}
+
+export default App;
